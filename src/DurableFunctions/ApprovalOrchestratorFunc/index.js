35aef4de6fb5e5669b13cc2ea6bbffffebc12d4a
Dan Gavrila <dan.gavrila3@gmail.com>
2023-07-05 16:01:32 +0300

    Add AZ Durable Function workflow sample.

---
+++ src/DurableFunctions/ApprovalOrchestratorFunc/index.js
@@ -0,0 +1,25 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    /*
+    * We will call the approval activity with a reject and an approved to simulate both
+    */
+
+    outputs.push(yield context.df.callActivity("ApprovalActivityFunc", "Approved"));
+    outputs.push(yield context.df.callActivity("ApprovalActivityFunc", "Rejected"));
+
+    return outputs;
+});
