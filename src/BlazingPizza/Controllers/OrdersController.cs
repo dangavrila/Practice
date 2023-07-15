63bce468827108eecd8ee641aaecd7706e04b6f4
Dan Gavrila <dan.gavrila3@gmail.com>
2023-07-15 15:24:04 +0300

    BlazingPizza: add Checkout page

---
+++ src/BlazingPizza/Controllers/OrdersController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlazingPizza.Data;
+
+namespace BlazingPizza.Controllers;
+
+[Route("orders")]
+[ApiController]
+public class OrdersController: Controller{
+    private readonly IDbContextFactory<PizzaStoreContext> _dbContextFactory;
+
+    public OrdersController(IDbContextFactory<PizzaStoreContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<OrderWithStatus>> GetOrders(){
+        List<OrderWithStatus> result = new();
+        var orders = (IEnumerable<Order>) null;
+        using (var context = _dbContextFactory.CreateDbContext()){
+            var ordersQuery = context.Orders
+            .Include(o => o.Pizzas).ThenInclude(o => o.Special)
+            .Include(o => o.Pizzas).ThenInclude(o => o.Toppings).ThenInclude(o => o.Topping)
+            .OrderByDescending(o => o.CreatedTime);
+
+            foreach(var order in ordersQuery){
+                var orderWithStatus = OrderWithStatus.FromOrder(order);
+                result.Add(orderWithStatus);
+            }
+        }
+        result = orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
+        return new OkObjectResult(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> PlaceOrder(Order order)
+    {
+        order.CreatedTime = DateTime.Now;
+
+        // Enforce existence of Pizza.SpecialId and Topping.ToppingId
+        // in the database - prevent the submitter from making up
+        // new specials and toppings
+        foreach (var pizza in order.Pizzas)
+        {
+            pizza.SpecialId = pizza.Special.Id;
+            pizza.Special = null;
+        }
+
+        await using (var context = _dbContextFactory.CreateDbContext()){
+            context.Orders.Attach(order);
+            await context.SaveChangesAsync();
+        }
+
+        return order.OrderId;
+    }
+}
--- src/BlazingPizza/Data/PizzaStoreContext.cs
+++ src/BlazingPizza/Data/PizzaStoreContext.cs
@@ -14,15 +14,29 @@ public class PizzaStoreContext : DbContext
     }
 
     public virtual DbSet<PizzaSpecial> PizzaSpecials { get; set; }
+    public virtual DbSet<Order> Orders {get;set;}
+    public virtual DbSet<Pizza> Pizzas { get; set; }
+    public virtual DbSet<Topping> Toppings {get;set;}
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={DbPath}");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.Entity<PizzaSpecial>()
             .ToTable("PizzaSpecials");
+        
+        // Configuring a many-to-many special -> topping
+        modelBuilder.Entity<PizzaTopping>()
+            .HasKey("PizzaId", "ToppingId");
+        modelBuilder.Entity<PizzaTopping>()
+            .HasOne<Pizza>()
+            .WithMany(ps => ps.Toppings);
+        modelBuilder.Entity<PizzaTopping>()
+            .HasOne(pst => pst.Topping)
+            .WithMany();
 
-        base.OnModelCreating(modelBuilder);
     }
 }
